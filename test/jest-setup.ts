d26a16384352a8e4d93f6bab73f489372ce9cf21
Thiago Santos <thiago.santos@maxmilhas.com.br>
2022-11-09 06:12:08 -0300

    test: fixing tests

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -10,15 +10,17 @@ declare global {
 	function getNames<T extends object>(c: { prototype: T }): T;
 }
 
-global.getNames = function getNames<T extends object>(c: { prototype: T }): T {
-	return new Proxy(c.prototype, {
-		get(target: T, property: string) {
-			const result = target[property as keyof T];
-			if (!result) {
-				throw new Error(`Method ${property} doesn't exist`);
-			}
+Object.assign(global, {
+	getNames: function getNames<T extends object>(c: { prototype: T }): T {
+		return new Proxy(c.prototype, {
+			get(target: T, property: string) {
+				const result = target[property as keyof T];
+				if (!result) {
+					throw new Error(`Method ${property} doesn't exist`);
+				}
 
-			return result;
-		},
-	});
-};
+				return result;
+			},
+		});
+	},
+});
